d9b349d005822d24cdf6ed5b2f8d7fdbb98f0ecf
Olia Kravets <94845931+helga20@users.noreply.github.com>
2023-03-09 21:47:11 +0200

    Add files via upload

---
+++ StudyWithPatron.DAL/Contex/PatronContex.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace StudyWithPatron.DAL.Contex
+{
+    public class PatronContext
+    {
+        public NpgsqlConnection CreateConnection()
+        {
+            string connectionString = "Host=127.0.0.1;Port=5432;User Id=postgres;Password=123123;Database=patron;CommandTimeout = 150;";
+            NpgsqlConnection marketConnection = new NpgsqlConnection(connectionString);
+            return marketConnection;
+        }
+
+    }
+
+}
