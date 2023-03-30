4f39b529ccb83cda0f6ce5a73d9e830854ac5d11
Olia Kravets <94845931+helga20@users.noreply.github.com>
2023-03-30 20:15:53 +0300

    Delete StudyWithPatron.DAL directory

--- StudyWithPatron.DAL/Repositories/Interface/IScoreBombsRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using StudyWithPatron.DAL.Models;
-
-namespace StudyWithPatron.DAL.Repositories.Interface
-{
-    public interface IScoreBombsRepository : IRepository<ScoreBombs> { }
-}
-
-
--- StudyWithPatron.DAL/Repositories/Interface/IUsersRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using StudyWithPatron.DAL.Models;
-
-namespace StudyWithPatron.DAL.Repositories.Interface
-{
-    public interface IUsersRepository : IRepository<Users> { }
-}
-
-
-
