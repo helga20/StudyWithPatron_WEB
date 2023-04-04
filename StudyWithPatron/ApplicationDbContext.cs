f7233c3a88655de6c4f8da62ceecb9f9c92f4e05
Olia Kravets <94845931+helga20@users.noreply.github.com>
2023-04-04 12:06:07 +0300

    Add files via upload

---
+++ StudyWithPatron/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyWithPatron.Models;
+
+namespace StudyWithPatron
+{
+    public class ApplicationDbContext : DbContext
+    {
+       // string connectionString = "Data Source=Study;Initial Catalog=Study;User ID=postgres;Password=oliak2003;Connect Timeout=150;Encrypt=False;";
+
+        public DbSet<ScoreBombs> ScoreBomb { get; set; }
+
+        public DbSet<ScoreUsers> ScoreUser { get; set; }
+
+        public DbSet<Users> User { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+
+}
+
+
---
+++ StudyWithPatron/AutoMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StudyWithPatron.Models;
+using StudyWithPatron.Dto;
+namespace StudyWithPatron
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<Users, UsersDto>();
+            CreateMap<UsersDto, Users>();
+
+            CreateMap<ScoreBombs, ScoreBombsDto>();
+            CreateMap<ScoreBombsDto, ScoreBombs>();
+
+            CreateMap<ScoreUsers, ScoreUsersDto>();
+            CreateMap<ScoreUsersDto, ScoreUsers>();
+        }
+    }
+}
